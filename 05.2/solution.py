8fa100c8b7655bb83b31c8a8b2daf34f1c962421
Arthur Busser <arthur.busser@hotmail.fr>
2017-12-06 01:17:05 +0100

    05.2: add solution

---
+++ 05.2/solution.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+def solve(input_file):
+    with open(input_file) as file:
+        input = [ int(line) for line in file ]
+
+    i = 0
+    count = 0
+
+    while i >= 0 and i < len(input):
+        offset = input[i]
+        input[i] += 1 if offset < 3 else -1
+        i += offset
+        count += 1
+
+    return count
+
+def _test_solve():
+    test_values = [
+        ("05.2/test_input_01.txt", 10),
+    ]
+    for (input, output) in test_values:
+        assert solve(input) == output
+
+_test_solve()
+print(solve("05.2/input.txt"))
