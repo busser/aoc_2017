802b651d57f4bc3083ddaa5bba8b7786c1d39684
Arthur Busser <arthur.busser@hotmail.fr>
2017-12-18 09:34:58 +0100

    13.1: add solution

---
+++ 13.1/solution.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+def solve(input_file):
+    with open(input_file) as file:
+        input = {
+            int(line.split()[0][:-1]):
+            int(line.split()[1])
+            for line in file
+        }
+
+    severity = 0
+
+    for depth, range in input.items():
+
+        # constant-time compute of wether there is a collision
+        collided = (
+            range - 1 - abs(depth % (2 * (range - 1)) - (range - 1))
+        ) == 0
+
+        if collided:
+            severity += depth * range
+
+    return severity
+
+def _test_solve():
+    test_values = [
+        ("13.1/test_input_01.txt", 24),
+    ]
+    for  (input, output) in test_values:
+        assert solve(input) == output
+
+_test_solve()
+print(solve("13.1/input.txt"))
